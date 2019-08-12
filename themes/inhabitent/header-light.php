e941bdfe232f4fcc29bb81dfa8fd32a71ce45f67
Kayla <kaynuhe@gmail.com>
2019-08-11 20:44:14 -0400

    feat: light and dark header on appropriate pages, contact form 7 styling

--- themes/inhabitent/header-light.php
+++ themes/inhabitent/header-light.php
@@ -12,12 +12,10 @@
 </head>
 <!-- add a diff class name depending on which page we're on -->
 <body <?php body_class(); ?>> 
+
     <header class="header-green">
         <img src="<?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-tent.svg" alt="tent logo">
         <?php wp_nav_menu( array(
             'theme_location' => 'primary'
         )); ?>
     </header>
-
-    <!-- <h1><?php bloginfo('name'); ?> -->
-
--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -1,20 +1,24 @@
 <?php get_header(); ?>
 
-<?php
-if( have_posts() ) :   // checks if pages are available if true, on to loop
-    // THE WP LOOP
-    while( have_posts() ) :   // while there are posts, runs as many times as are posts
-    the_post(); ?>          <!-- load all posts, needed to make loop run -->
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
 
-    <!-- end of wp loop -->
-    <?php endwhile; ?>
+<main class="page-content-wrapper">
+    <?php
+    if( have_posts() ) :   // checks if pages are available if true, on to loop
+        // THE WP LOOP
+        while( have_posts() ) :   // while there are posts, runs as many times as are posts
+        the_post(); ?>          <!-- load all posts, needed to make loop run -->
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
 
-<?php the_posts_navigation(); ?> <!-- if too much content to load on page - can change amount in settings -->
+        <!-- end of wp loop -->
+        <?php endwhile; ?>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+    <?php the_posts_navigation(); ?> <!-- if too much content to load on page - can change amount in settings -->
+
+    <?php else : ?>
+            <p>No posts found</p>
+    <?php endif; ?>
+</main>
+
 
 <?php get_footer(); ?>
