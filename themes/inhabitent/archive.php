1d234c54f270b9ac09f2a835308b3d89bb6d4d82
Kayla <kaynuhe@gmail.com>
2019-08-21 14:32:45 -0400

    feat: add archive.php and style like journal page

---
+++ themes/inhabitent/archive.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<main class="page-sidebar-content-wrapper all-page-wrapper">
+    <section class="posts-content-wrapper">
+        <?php
+        if( have_posts() ) :   
+            // THE WP LOOP
+            while( have_posts() ) :   
+            the_post(); ?>         
+
+        <div class="journal-post-head">
+            <h2 class="journal-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="featured-image-wrapper journal-img-wrapper">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <h4><?php the_date('j F Y'); ?> / <?php echo get_comments_number(); ?> Comments / By <?php the_author(); ?></h4>
+            <?php endif; ?>
+           
+        </div>
+        
+        <div class="journal-post-text">
+            <?php the_excerpt(); ?>
+        </div>
+       
+            <?php endwhile; ?>
+
+        <?php the_posts_navigation(); ?> 
+
+        <?php else : ?>
+                <p>No posts found</p>
+        <?php endif; ?>
+    </section>
+    <section class="the-sidebar">
+        <?php dynamic_sidebar('sidebar-1'); ?>
+    </section>
+    
+</main>
+
+
+<?php get_footer(); ?>
+
+                        
