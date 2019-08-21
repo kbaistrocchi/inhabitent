abebd78f91dbb6ca70c6a3c3f421ac141fcb3cd7
Kayla <kaynuhe@gmail.com>
2019-08-21 14:53:38 -0400

    style: search.php

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -1,23 +1,46 @@
-<!-- wp goes into database, locates title, content and outputs
-into here. Know as the loop -->
 <?php get_header(); ?>
 
-<?php
-if( have_posts() ) :   // checks if posts are available if true, on to loop
-    // THE WP LOOP
-    while( have_posts() ) :   // while there are posts, runs as many times as are posts
-    the_post(); ?>          <!-- load all posts, needed to make loop run -->
-<h2><?php the_title(); ?></h2>
-<!-- <?php the_content(); ?> -->
+<main class="page-sidebar-content-wrapper all-page-wrapper">
+    <section class="posts-content-wrapper">
+    <div class="four-0-four-search-form four-0-four-block">
+            <?php get_search_form();?>
+    </div>
+    
+        <?php
+        if( have_posts() ) :   
+            // THE WP LOOP
+            while( have_posts() ) :   
+            the_post(); ?>         
 
-    <!-- end of wp loop -->
-    <?php endwhile; ?>
+        <div class="journal-post-head">
+            <h2 class="journal-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="featured-image-wrapper journal-img-wrapper">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <h4><?php the_date('j F Y'); ?> / <?php echo get_comments_number(); ?> Comments / By <?php the_author(); ?></h4>
+            <?php endif; ?>
+           
+        </div>
+        
+        <div class="journal-post-text">
+            <?php the_excerpt(); ?>
+        </div>
+       
+            <?php endwhile; ?>
 
-<?php the_posts_navigation(); ?> <!-- if too much content to load on page - can change amount in settings -->
+        <?php the_posts_navigation(); ?> 
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+        <?php else : ?>
+                <p>No posts found</p>
+        <?php endif; ?>
+    </section>
+    <section class="the-sidebar">
+        <?php dynamic_sidebar('sidebar-1'); ?>
+    </section>
+    
+</main>
+
 
 <?php get_footer(); ?>
 
